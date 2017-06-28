772b2c5c8e6e591ea64edb2e6eac7d8fe2cd0385
Pedro Castanha <pedro.castanha@softplan.com.br>
2017-06-28 14:43:55 -0300

    - New constants for different execution environments.

--- src/softframe/classification/lib.py
+++ src/softframe/classification/lib.py
@@ -1,7 +1,7 @@
-# TRAINING_MODELS_DIR = "../misc/resources/models/"
-# ROUTINES_MODELS_DIR = "resources/models/"
-MODELS_DIR = "src/softframe/misc/resources/models/"
-# CMD_LINE_MODELS_DIR = "src/softframe/misc/resources/models/" # From command line execution this should be the path
+TRAINING_MODELS_DIR = "../misc/resources/models/"
+ROUTINES_MODELS_DIR = "resources/models/"
+CMD_LINE_MODELS_DIR = "src/softframe/misc/resources/models/"  # From command line execution this should be the path
+MODELS_DIR = CMD_LINE_MODELS_DIR
 CLASSIFICADOR_INICIAL = "peticao_inicial"
 CLASSIFICADOR_INICIAL_MODELO = "classificacao_iniciais.pkl"
 
